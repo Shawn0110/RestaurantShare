eb1381334ccbabfd50c999fbdec61d440fe32432
Shawn <64045480+sl0110@users.noreply.github.com>
2020-07-15 12:06:31 -0400

    herro

--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import *
-
 # Create your views here.
 def index(request):
     categories = Category.objects.all()
-    content = {'categories' : categories}
+    restaurants = Restaurant.objects.all()
+    content = {'categories': categories, 'restaurants': restaurants}
     # return HttpResponse("index")
     return render(request, 'shareRes/index.html', content)
+
 
 def restaurantDetail(request):
     #return HttpResponse("restaurantDetail")
@@ -19,6 +20,17 @@ def restaurantCreate(request):
     content = {'categories' : categories}
     #return HttpResponse("restaurantCreate")
     return render(request, 'shareRes/restaurantCreate.html', content)
+
+def Create_restaurant(request):
+    category_id = request.POST['resCategory']
+    category = Category.objects.get(id = category_id)
+    name = request.POST['resTitle']
+    link = request.POST['resLink']
+    content = request.POST['resContent']
+    keyword = request.POST['resLoc']
+    new_res = Restaurant(category = category, restaurant_name = name, restaurant_link = link, restaurant_content = content, restaurant_keyword = keyword)
+    new_res.save()
+    return HttpResponseRedirect(reverse('index'))
 
 def categoryCreate(request):
     categories = Category.objects.all()
