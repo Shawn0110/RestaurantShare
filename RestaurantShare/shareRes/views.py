e4a2b843ccf1ae0280d2581808baa0cf1888a464
Shawn <64045480+sl0110@users.noreply.github.com>
2020-07-15 10:35:57 -0400

    hi

--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -1,26 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
 # Create your views here.
 def index(request):
+    categories = Category.objects.all()
+    content = {'categories' : categories}
     # return HttpResponse("index")
-    return render(request, 'shareRes/index.html')
+    return render(request, 'shareRes/index.html', content)
 
 def restaurantDetail(request):
     #return HttpResponse("restaurantDetail")
     return render(request, 'shareRes/restaurantDetail.html')
 
 def restaurantCreate(request):
+    categories = Category.objects.all()
+    content = {'categories' : categories}
     #return HttpResponse("restaurantCreate")
-    return render(request, 'shareRes/restaurantCreate.html')
+    return render(request, 'shareRes/restaurantCreate.html', content)
 
 def categoryCreate(request):
-    #return HttpResponse("categoryCreate")
-    return render(request, 'shareRes/categoryCreate.html')
+    categories = Category.objects.all()
+    content = {'categories': categories}
+    # return HttpResponse("categoryCreate")
+    return render(request, 'shareRes/categoryCreate.html', content)
+
 
 def Create_category(request):
     category_name = request.POST['categoryName']
-    new_category = Category(cagetory_name = category_name)
+    new_category = Category(category_name = category_name)
     new_category.save()
-    return HttpResponse
+    return HttpResponseRedirect(reverse('index'))
     #return HttpResponse("여기서 category Create 기능을 구현")
+    
+def Delete_category(request):
+    category_id = request.POST['categoryId']
+    delete_category = Category.objects.get(id = category_id)
+    delete_category.delete()
+    return HttpResponseRedirect(reverse('cateCreatePage'))
